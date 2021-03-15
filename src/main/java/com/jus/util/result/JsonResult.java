40c5aab423709d0e489a46fb6eb1d490b9671727
JusChui <1473441767@qq.com>
2021-03-15 14:41:56 +0800

    Restful Json

---
+++ src/main/java/com/jus/util/result/JsonResult.java
@@ -0,0 +1,28 @@
+package com.jus.util.result;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName JsonResult.java
+ * @author: JusChui
+ * @Description:
+ * @createTime: 2021/3/15 14:18
+ */
+@Data
+public class JsonResult implements Serializable {
+
+    private String returnCode;
+
+    private String returnMessage;
+
+    private Map<String, Object> bean;
+
+    private List<Map<String, Object>> beans;
+
+    private Object data;
+
+}
